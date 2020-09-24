7a0e088f724087fdfe76e8d2aea4fada6bc2b497
Rodrigo Gryzinski <rogryza@rogryza.com>
2020-09-24 07:10:57 -0300

    Check changes in location and image_id

--- nixops_hetznercloud/backends/hetznercloud.py
+++ nixops_hetznercloud/backends/hetznercloud.py
@@ -6,13 +6,16 @@ import hcloud
 from hcloud.images.domain import Image
 from hcloud.server_types.domain import ServerType
 from hcloud.servers.domain import Server
+from hcloud.ssh_keys.domain import SSHKey
 from nixops.backends import MachineDefinition, MachineOptions, MachineState
 from nixops.resources import ResourceOptions
 from nixops.util import attr_property
 
 
 class HetznerCloudVmOptions(ResourceOptions):
-    image: int
+    image: Optional[int]
+    # TODO validate image_selector
+    image_selector: str
     location: str
     serverType: str
     token: Optional[str]
@@ -38,8 +41,11 @@ class HetznerCloudState(MachineState[HetznerCloudDefinition]):
         return "hetznercloud"
 
     state = attr_property("state", MachineState.MISSING, int)  # override
-    public_ipv4 = attr_property("publicIpv4", None)
-    token = attr_property("hetznercloud.token", None)
+    public_ipv4 = attr_property("publicIpv4", None, str)
+    token = attr_property("hetznercloud.token", None, str)
+    image_id = attr_property("hetznercloud.image_id", None, int)
+    location = attr_property("hetznercloud.location", None, str)
+    server_type = attr_property("hetznercloud.server_type", None, str)
 
     def __init__(self, depl, name, id):
         super().__init__(depl, name, id)
@@ -56,6 +62,7 @@ class HetznerCloudState(MachineState[HetznerCloudDefinition]):
         assert isinstance(defn, HetznerCloudDefinition)
         if self.state not in (self.RESCUE, self.UP) or check:
             self.check()
+
         self.set_common_state(defn)
 
         if defn.config.hetznercloud.token is None:
@@ -68,6 +75,24 @@ class HetznerCloudState(MachineState[HetznerCloudDefinition]):
 
         client = self._client()
         hetzner = defn.config.hetznercloud
+        image_id = self._fetch_image_id(hetzner.image, hetzner.image_selector)
+        if self.image_id is None:
+            self.image_id = image_id
+        elif self.image_id != image_id:
+            self.warn(
+                f"image_id changed from {self.image_id} to {image_id} but can't update image of a VM."
+            )
+        if self.location is None:
+            self.location = hetzner.location
+        elif self.location != hetzner.location:
+            self.warn(
+                f"location changed from {self.location} to {hetzner.location} but can't update location of a VM."
+            )
+        if hetzner.serverType != self.server_type:
+            # TODO update server type
+            pass
+        self.server_type = hetzner.serverType
+
         if not self.vm_id:
             self.log(
                 "Creating Hetzner Cloud VM ("
@@ -76,8 +101,15 @@ class HetznerCloudState(MachineState[HetznerCloudDefinition]):
             )
             response = client.servers.create(
                 name=self.name,
-                server_type=ServerType(hetzner.serverType),
-                image=Image(id=hetzner.image, type="snapshot"),
+                # TODO manage SSH keys correcly
+                keys=SSHKey(name="admin"),
+                server_type=ServerType(self.server_type),
+                image=Image(id=self.image_id),
+                # Set labels so we can find the instance if nixops crashes before writing vm_id
+                labels={
+                    "nixops_hetznercloud/name": self.name,
+                    "nixops_hetznercloud/deployment": self.depl.uuid,
+                },
             )
             with self.depl._db:
                 self.vm_id = response.server.id
@@ -89,10 +121,9 @@ class HetznerCloudState(MachineState[HetznerCloudDefinition]):
             return True
         if wipe:
             self.warn("Wipe is not supported")
-        self.log_start("destroying Hetzner Cloud VM...")
+        self.log("destroying Hetzner Cloud VM...")
         client = self._client()
         client.servers.delete(Server(id=self.vm_id))
-        self.log_end("")
         return True
 
     def get_ssh_flags(self, *args, **kwargs):
@@ -107,10 +138,24 @@ class HetznerCloudState(MachineState[HetznerCloudDefinition]):
         return self.public_ipv4
 
     def _check(self, res):
-        if not self.vm_id:
+        # TODO better state checking
+        if self.vm_id is None:
             res.exists = False
             return
         res.exists = True
         res.is_up = self.ping()
         if res.is_up:
             super()._check(res)
+
+    def _fetch_image_id(self, image: Optional[int], image_selector: str) -> int:
+        client = self._client()
+        if image is None:
+            self.log(f"Finding image matching {image_selector}...")
+            matches, _ = client.images.get_list(
+                label_selector=image_selector, sort="created:desc",
+            )
+            if len(matches) == 0:
+                raise Exception(f"No images found matching {image_selector}")
+            return matches[0].image.id
+        else:
+            return image
--- nixops_hetznercloud/nix/hetznercloud.nix
+++ nixops_hetznercloud/nix/hetznercloud.nix
@@ -6,14 +6,26 @@ with lib;
 
   options.deployment.hetznercloud = {
     image = mkOption {
-      type = types.int;
+      type = types.nullOr types.int;
       description = ''
         Image ID to use for this VM. It's expected to be a NixOs system with SSH access to root.
+        Takes precedence over <option>deployment.hetznercloud.image_selector</option>.
+      '';
+    };
+
+    image_selector = mkOption {
+      type = types.str;
+      default = "nixops_hetznercloud";
+      description = ''
+        <link xlink:href='https://docs.hetzner.cloud/#label-selector'>Label selector</link> for the
+        server image. If multiple images are found, the most recent one will be used.
+        The image is expected to be a NixOs system with SSH access to root.
       '';
     };
 
     location = mkOption {
       type = types.str;
+      example = "fsn1";
       description = ''
         VM location name.
       '';
@@ -43,6 +55,11 @@ with lib;
 
   config = mkIf (config.deployment.targetEnv == "hetznercloud") {
     nixpkgs.system = mkOverride 900 "x86_64-linux";
+
+    # TODO where do imports go?
+    # imports = [
+    #   <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
+    # ];
 
     boot.initrd.availableKernelModules = [ "ata_piix" "virtio_pci" "xhci_pci" "sd_mod" "sr_mod" ];
     boot.initrd.kernelModules = [ ];
